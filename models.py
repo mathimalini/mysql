8919846e47f43e673cf1f7a18912a47143012a85
mathimalini <mathiyarasi1304@gmail.com>
2024-09-25 13:26:55 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class RegisterForm(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    address=models.CharField(max_length=100)
+    contact=models.CharField(max_length=50)
+    email=models.EmailField()
+    
+    
+    class Meta:
+        db_table='datas'
+        
+    
